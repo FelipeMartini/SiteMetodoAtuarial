fd568742ff875f9243f77f2c1607d6c1f23e9f75
FelipeMartini <felipemartinii@gmail.com>
2025-08-02 19:30:19 -0300

    VERSÃO FINAL BOA: Configuração de testes com SWC/Jest para Next.js moderno. TODOS OS TESTES PASSANDO. ESTA É UMA VERSÃO BOA E ESTÁVEL.

--- nextjs-app/jest.config.js
+++ nextjs-app/jest.config.js
@@ -1,9 +1,13 @@
 module.exports = {
   // Configuração do ambiente de testes para React/Next.js
-  testEnvironment: "jest-environment-jsdom", // Corrigido para o nome do módulo instalado
-  setupFilesAfterEnv: ["@testing-library/jest-dom"], // Caminho atualizado conforme recomendação oficial
+  testEnvironment: "jest-environment-jsdom", // Simula o DOM para testes de componentes React
+  setupFilesAfterEnv: ["@testing-library/jest-dom"], // Extensões do Testing Library para asserções
   moduleNameMapper: {
-    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy", // Evita erro ao importar estilos
   },
-  testPathIgnorePatterns: ["/node_modules/", "/.next/"],
+  testPathIgnorePatterns: ["/node_modules/", "/.next/"], // Ignora pastas desnecessárias
+  // Utiliza o SWC como transformador para arquivos TypeScript/JSX, compatível com Next.js moderno
+  transform: {
+    "^.+\\.(t|j)sx?$": ["@swc/jest"]
+  },
 };
