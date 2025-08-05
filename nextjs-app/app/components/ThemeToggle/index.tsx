dbf1c8ac884d502687616d25f762d004721f8d26
FelipeMartini <felipemartinii@gmail.com>
2025-08-05 12:53:35 -0300

    ..

--- nextjs-app/app/components/ThemeToggle/index.tsx
+++ nextjs-app/app/components/ThemeToggle/index.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React from 'react';
-import { useTema } from '../contexts/ThemeContext';
+import { useTema } from '../../contexts/ThemeContext';
 import {
   ThemeToggleContainer,
   ThemeLabel,
