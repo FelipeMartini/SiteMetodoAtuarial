96c155002468ef9a6118ded7331b0816c805dd3e
FelipeMartini <felipemartinii@gmail.com>
2025-08-11 22:28:46 -0300

    clean

--- site-metodo/src/__tests__/auth/flow.test.ts
+++
@@ -1,8 +0,0 @@
-/**
- * TODO: Corrigir testes após migração Auth.js v5 completa
- */
-
-// Testes temporariamente desabilitados durante migração
-describe.skip('Auth.js catch-all handler', () => {
-  // Testes serão reativados após implementação completa
-})
